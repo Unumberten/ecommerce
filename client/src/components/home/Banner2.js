f10ba3807828deed7dc1bd1cca02f411a59e5a28
Unumberten <honestgceba@gmail.com>
2024-07-26 09:01:11 +0200

    Update Banner2.js

--- client/src/components/home/Banner2.js
+++ client/src/components/home/Banner2.js
@@ -38,14 +38,14 @@ const Banner2 = ({ className }) => {
           <article className="flex flex-col justify-start items-end order-1">
             <div className="flex flex-col gap-y-4 max-w-lg z-50 lg:ml-auto lg:mr-0 mr-auto">
               <h1 className="md:text-6xl text-4xl">
-                Special offer in kids products
+                Special Offer for Produce in the Taung Area
               </h1>
               <p className="flex flex-row gap-x-0.5 items-center text-lg text-slate-500">
-                Fashion is a form of self-expression and autonomy at a
+                For the best prices to form of self-expression and autonomy at a
                 particular period and place.
               </p>
               <button className="px-8 py-4 border border-black rounded-secondary bg-black hover:bg-black/90 text-white transition-colors drop-shadow w-fit mt-4"
-              onClick={() => router.push("https://devhasibulislam.vercel.app/")}>
+              onClick={() => router.push("https://mafihub.co.za/")}>
                 Discover More
               </button>
             </div>
