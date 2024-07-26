a04d25299a15bb22c63546ee3d1dbb8e7a6bfc72
Unumberten <honestgceba@gmail.com>
2024-07-26 09:11:14 +0200

    Update Banner3.js

--- client/src/components/home/Banner3.js
+++ client/src/components/home/Banner3.js
@@ -36,9 +36,9 @@ const Banner3 = ({ className }) => {
         />
         <article className="flex flex-col justify-start items-end order-1">
           <div className="flex flex-col gap-y-4 max-w-lg z-50 lg:mr-auto lg:mr-0 mr-auto">
-            <h1 className="md:text-6xl text-4xl">Earn free money with Canim</h1>
+            <h1 className="md:text-6xl text-4xl">Be a seller on Farmbuy</h1>
             <p className="flex flex-row gap-x-0.5 items-center text-lg text-slate-500">
-              With Ciseco you will get free-shipping & savings combo.
+              Bring your Produce and sell on the best marketplace. 
             </p>
             <button
               className="px-8 py-4 border border-black rounded-secondary bg-black hover:bg-black/90 text-white transition-colors drop-shadow w-fit mt-4"
